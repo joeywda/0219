47e3e5762e39e83dd62a42b4970ac250e33ede4b
unknown <joey.wda@gmail.com>
2020-02-22 23:50:11 +0800

    1st

---
+++ rpg.js
@@ -0,0 +1,23 @@
+var player=new Object();
+player.name="john";
+player.hp=100;
+player.fight=function(){
+    this.hp=this.hp-2;
+}
+player.rest=function(){
+    this.hp++;
+}
+player.report=function(){
+    console.log(this.name+":"+this.hp);
+}
+
+player.fight();
+player.rest();
+player.report();
+
+// echo "# 0219" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit2"
+// git remote add origin https://github.com/joeywda/0219.git
+// git push -u origin master
---
+++ sort.js
@@ -0,0 +1,11 @@
+var arr = new Array(6)
+arr[0] = "George"
+arr[1] = "John"
+arr[2] = "Thomas"
+arr[3] = "1"
+arr[4] = "Adrew"
+arr[5] = "Martin"
+
+// document.write(arr + "<br />")
+// document.write(arr.sort())
+console.log(arr.sort());
